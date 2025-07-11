a391b925d6c11c28a3c7badcf84c9fa9423fb354
LGJ0405 <107593225+LGJ0405@users.noreply.github.com>
2025-07-11 14:18:14 +0900

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,3 @@ def api_key():
     uddi = "4b80ee12-8cb5-4ac9-b08b-fd58b7dac635"
     serviceKey = '3t3FAyhLyfK85gktC0YtQ61/Xw6Ji9QL1DDZXUDDvnkjJJtuNc2FLwORbAklsL6HeNyZUPk89NZycJcPWauPQg=='
     return user_id, uddi, serviceKey
-
-
-/Users/woojin/Desktop/SK Networks Family AI Camp_17/1차_5팀_NOESIS
